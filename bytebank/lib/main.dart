4ab80819813373f38b72e100f862449884c02131
Paulo Roberto <betinho5444@gmail.com>
2020-05-04 16:20:08 -0300

    Refatorando código

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -5,23 +5,43 @@ void main() => runApp(MaterialApp(
       appBar: AppBar(
         title: Text("Transferências"),
       ),
-      body: Column(children: <Widget>[
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text("10.0"),
-            subtitle: Text('1000'),
-          )
-        ),
-         Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text("10.0"),
-            subtitle: Text('1000'),
-          )
-        ),
-      ],),
+      body: ListaTransferencias(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
     )));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+        ItemTransferencia(Transferencia('100', '1000')),
+        ItemTransferencia(Transferencia('200', '1000')),
+        ItemTransferencia(Transferencia('300', '1000')),
+      ],);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia
+
+  ItemTransferencia( _transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text( _transferencia.valor),
+        subtitle: Text( _transferencia.numeroConta),
+      )
+    );
+  }
+}
+
+class Transferencia {
+  final String valor;
+  final String numeroConta;
+
+  Transferencia(this.valor, this.numeroConta)
+}
